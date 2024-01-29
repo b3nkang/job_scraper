e88fc07386f8ac8ddd76a61aab9e4611db71630d
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-01-29 15:27:24 -0500

    add preliminarily functional scraper

--- job_scraper.py
+++ job_scraper.py
@@ -7,60 +7,43 @@ from dotenv import load_dotenv
 import os
 import pytest
 
-
 load_dotenv()
 
 api_key = os.getenv('OPENAI_API_KEY')
 client = instructor.patch(OpenAI(api_key=api_key))
 
-# class JobDetails(BaseModel):
-#     job_title : str
-#     job_url : str
-#     company_id : int
-#     city : str
-#     state : str
-#     country : str
-#     work_arrangement : str
-#     salary_lower_bound : str
-#     salary_upper_bound : str
-#     salary_frequency : str
-#     min_qualifications : str
-
-# job: JobDetails = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     response_model=JobDetails,
-#     messages=[
-#         {"role": "user", "content": "Extract all possible/specified details for a given job listing of your choice"},
-#     ]
-# )
-
-class UserDetail(BaseModel):
-    name: str
-    age: int
-
-
-user: UserDetail = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    response_model=UserDetail,
-    messages=[
-        {"role": "user", "content": "Extract Jason is 25 years old"},
-    ]
-)
-
-assert user.name == "Jason"
-assert user.age == 25
-
-print(user)
+class JobDetails(BaseModel):
+    job_title : str
+    job_url : str
+    company_id : int
+    city : str
+    state : str
+    country : str
+    work_arrangement : str
+    salary_lower_bound : str
+    salary_upper_bound : str
+    salary_frequency : str
+    min_qualifications : str
 
 def scrape_job_posting(url):
     response = requests.get(url)
 
     if response.status_code == 200:
-        print("successfully fetched url")
-        
+        print("successfully fetched from url")
         soup = BeautifulSoup(response.text, "lxml")	# using lxml parser for speed + leniency
+        return soup
         
     else:
-        print(f"failed to fetch url, {response.status_code}")
+        print(f"failed to fetch from url, {response.status_code}")
 
-# scrape_job_posting('https://webscraper.io/test-sites/e-commerce/allinone')
+def extract_job_posting(html):
+    job: JobDetails = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        response_model=JobDetails,
+        messages=[
+            {"role": "user", "content": "Extract all given/specified details for the following job listing html page:" + str(html)},
+        ]
+    )
+    return job
+
+print(extract_job_posting(scrape_job_posting('https://webscraper.io/jobs')))
